5cfe69727289a32645c434018fa18ca3b53b7af0
Chintia Devi <hitmeup.defi@gmail.com>
2024-06-07 13:07:40 +0700

    Create layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route,
+} from 'react-router-dom';
+import {Layout, Typography, Button } from 'antd';
+
+import Dashboard from './views/dashboard';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const { Header, Footer } = Layout;
+const { Title } = Typography;
+
+function App() {
+  return (
+    <Layout className='h-screen'>
+    <Header className='flex justify-between items-center'>
+        <Title level={2} className='m-0 text-white font-extralight'><b>GitHub</b> Jobs</Title>
+        <Button>Login with Google</Button>
+    </Header>
+    <div className='bg-white h-full'>
+      <BrowserRouter>
+        <Routes>
+          <Route
+                path="/"
+                element={<Dashboard />}
+              />
+        </Routes>
+      </BrowserRouter>
+    </div>
+    <Footer style={{ textAlign: 'center' }}>
+        GitHub Jobs ©{new Date().getFullYear()}
+    </Footer>
+</Layout>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +41,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
