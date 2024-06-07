d4c6d20a1306550d46d2a031009b2d1ca069ddd9
Chintia Devi <hitmeup.defi@gmail.com>
2024-06-07 20:53:45 +0700

    Page List Jobs

--- src/index.js
+++ src/index.js
@@ -7,31 +7,32 @@ import {Layout, Typography, Button } from 'antd';
 
 import Dashboard from './views/dashboard';
 import './index.css';
+import styles from './index.module.css';
 
 const { Header, Footer } = Layout;
 const { Title } = Typography;
 
 function App() {
   return (
-    <Layout className='h-screen'>
-    <Header className='flex justify-between items-center'>
-        <Title level={2} className='m-0 text-white font-extralight'><b>GitHub</b> Jobs</Title>
-        <Button>Login with Google</Button>
-    </Header>
-    <div className='bg-white h-full'>
-      <BrowserRouter>
-        <Routes>
-          <Route
-                path="/"
-                element={<Dashboard />}
-              />
-        </Routes>
-      </BrowserRouter>
-    </div>
-    <Footer style={{ textAlign: 'center' }}>
-        GitHub Jobs ©{new Date().getFullYear()}
-    </Footer>
-</Layout>
+    <Layout className='h-screen overflow-hidden'>
+      <Header className='flex justify-between items-center'>
+          <Title level={2} className='m-0 text-white font-extralight'><b>GitHub</b> Jobs</Title>
+          <Button type='primary'>Login with Google</Button>
+      </Header>
+      <div className={styles.container}>
+        <BrowserRouter>
+          <Routes>
+            <Route
+                  path="/"
+                  element={<Dashboard />}
+                />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer style={{ textAlign: 'center' }}>
+          GitHub Jobs ©{new Date().getFullYear()}
+      </Footer>
+  </Layout>
   );
 }
 
---
+++ src/views/utils/moment.js
@@ -0,0 +1,54 @@
+import dayjs from 'dayjs';
+import isToday from 'dayjs/plugin/isToday';
+import isTomorrow from 'dayjs/plugin/isTomorrow';
+import isYesterday from 'dayjs/plugin/isYesterday';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import timezone from 'dayjs/plugin/timezone';
+import updateLocale from 'dayjs/plugin/updateLocale';
+import utc from 'dayjs/plugin/utc';
+
+dayjs.locale('id');
+
+dayjs.extend(isToday);
+dayjs.extend(isTomorrow);
+dayjs.extend(isYesterday);
+dayjs.extend(relativeTime);
+dayjs.extend(timezone);
+dayjs.extend(updateLocale);
+dayjs.extend(utc);
+
+dayjs.tz.setDefault('Asia/Jakarta');
+
+dayjs.updateLocale('en', {
+  weekdaysMin: ['Min', 'Sen', 'Sel', 'Rab', 'Kam', 'Jum', 'Sab'],
+  weekdays: ['Minggu', 'Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu'],
+  months: [
+    'Januari',
+    'Februari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember',
+  ],
+});
+
+export const FORMAT_DATE = 'DD MMMM YYYY, HH:mm';
+
+const IndonesiaTimezones = {
+  7: 'Asia/Jakarta',
+  8: 'Asia/Makassar',
+  9: 'Asia/Manokwari',
+};
+
+export const getCurrentTimeZone = () => {
+  const offset = dayjs().utcOffset() / 60;
+  return IndonesiaTimezones[offset] || dayjs.tz.guess();
+};
+
+export default dayjs;
